6e348c682c16397a8e8b6b69103a4e8a5beebef7
KOB-J <audiothingz@outlook.com>
2023-05-16 11:32:20 +0400

    Update MainComponent files

--- Sources/MainComponent.cpp
+++ Sources/MainComponent.cpp
@@ -4,22 +4,92 @@
 MainComponent::MainComponent()
 {
     setSize (600, 400);
+
+    addAndMakeVisible(brandLabel);
+    addAndMakeVisible(appNameLanel);
+
+    addAndMakeVisible(inputFilesLabel);
+    addAndMakeVisible(inputTextEditor);
+    addAndMakeVisible(inputBrowserButton);
+
+    addAndMakeVisible(outputFolderLabel);
+    addAndMakeVisible(outputTextEditor);
+    addAndMakeVisible(outputBrowserButton);
+
+    addAndMakeVisible(proceedButton);
+
+    brandLabel.setText("Juqa.Solutions", juce::dontSendNotification);
+    appNameLanel.setText("PNG TO FILMSTRIP", juce::dontSendNotification);
+    inputFilesLabel.setText("Select the folder containing the .png files:", juce::dontSendNotification);
+    inputTextEditor.setText(fileToLoad.getFullPathName());
+    outputFolderLabel.setText("Select the output folder:", juce::dontSendNotification);
+    outputTextEditor.setText(fileToSave.getFullPathName());
+    proceedButton.setButtonText("Let's go!");
+
+    inputBrowserButton.onClick = [&] {launchBrowser("Select the folder containing the .png files:", inputTextEditor); };
+    outputBrowserButton.onClick = [&] {launchBrowser("Select the output folder:", outputTextEditor); };
+    proceedButton.onClick = [&] {proceed();};
+
+    //TODO Put Patreon link
+}
+
+MainComponent::~MainComponent()
+{
 }
 
 //==============================================================================
 void MainComponent::paint (juce::Graphics& g)
 {
-    // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
-
-    g.setFont (juce::Font (16.0f));
-    g.setColour (juce::Colours::white);
-    g.drawText ("Hello World!", getLocalBounds(), juce::Justification::centred, true);
 }
 
 void MainComponent::resized()
 {
-    // This is called when the MainComponent is resized.
-    // If you add any child components, this is where you should
-    // update their positions.
+    auto bounds = getLocalBounds();
+    auto height = bounds.getHeight() / 4;
+
+    auto headerBounds = bounds.removeFromTop(height);
+    auto inputBounds = bounds.removeFromTop(height);
+    auto outputBounds = bounds.removeFromTop(height);
+    auto footerBounds = bounds.removeFromTop(height);
+
+    brandLabel.setBounds(headerBounds.removeFromLeft(headerBounds.getWidth() / 2));
+    appNameLanel.setBounds(headerBounds);
+
+    inputFilesLabel.setBounds(inputBounds.removeFromTop(inputBounds.getHeight() / 2));
+    inputTextEditor.setBounds(inputBounds.removeFromLeft(inputBounds.getWidth() * 0.9f));
+    inputBrowserButton.setBounds(inputBounds);
+
+    outputFolderLabel.setBounds(outputBounds.removeFromTop(outputBounds.getHeight() / 2));
+    outputTextEditor.setBounds(outputBounds.removeFromLeft(outputBounds.getWidth() * 0.9f));
+    outputBrowserButton.setBounds(outputBounds);
+
+    proceedButton.setBounds(footerBounds.removeFromRight(footerBounds.getWidth() / 2));
+}
+
+void MainComponent::launchBrowser(juce::String browserText, juce::TextEditor& textEditor)
+{
+    myChooser = std::make_unique<juce::FileChooser>(browserText,
+                                                    juce::File::getSpecialLocation(juce::File::userHomeDirectory),
+                                                    "*.png");
+
+    myChooser->launchAsync(
+            juce::FileBrowserComponent::openMode | juce::FileBrowserComponent::canSelectDirectories,
+            [&](const juce::FileChooser& chooser)
+            {
+                juce::File returnedFile(chooser.getResult());
+                textEditor.setText(returnedFile.getFullPathName());
+            });
+}
+
+void MainComponent::proceed()
+{
+    DBG("Poceed!");
+
+    /**
+    Verifications:
+
+        input:
+            folder contains .png files - height and width identical on all files - files are Bitmap type - ...
+    */
 }
--- Sources/MainComponent.h
+++ Sources/MainComponent.h
@@ -1,29 +1,33 @@
 #pragma once
 
-// CMake builds don't use an AppConfig.h, so it's safe to include juce module headers
-// directly. If you need to remain compatible with Projucer-generated builds, and
-// have called `juce_generate_juce_header(<thisTarget>)` in your CMakeLists.txt,
-// you could `#include <JuceHeader.h>` here instead, to make all your module headers visible.
 #include <juce_gui_extra/juce_gui_extra.h>
 
-//==============================================================================
-/*
-    This component lives inside our window, and this is where you should put all
-    your controls and content.
-*/
-class MainComponent   : public juce::Component
+class MainComponent  : public juce::Component
 {
 public:
     //==============================================================================
     MainComponent();
+    ~MainComponent() override;
 
     //==============================================================================
     void paint (juce::Graphics&) override;
     void resized() override;
 
+    void printToLog(juce::String message);
+
 private:
-    //==============================================================================
-    // Your private member variables go here...
+    juce::Label brandLabel, appNameLanel, inputFilesLabel, outputFolderLabel;
+    juce::TextButton proceedButton;
+    juce::ImageButton inputBrowserButton, outputBrowserButton;
+    juce::TextEditor inputTextEditor, outputTextEditor;
+
+    juce::File fileToLoad{ juce::File::getSpecialLocation(juce::File::userDocumentsDirectory).getFullPathName() + ("PNGToFilmstrip/pngToConvert") };
+    juce::File fileToSave{ juce::File::getSpecialLocation(juce::File::userDocumentsDirectory).getFullPathName() + ("PNGToFilmstrip/exportedPngFilmstrip/Output.png") };
+
+    std::unique_ptr<juce::FileChooser> myChooser;
+
+    void launchBrowser(juce::String browserText, juce::TextEditor& textEditor);
+    void proceed();
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
 };
